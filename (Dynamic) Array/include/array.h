f1f11b6eda95ba99f9ed566b02e2f22b6027f003
Ali Hamza <ali.mhamza06@gmail.com>
2025-10-31 21:22:39 +0400

    Extend array implementation with insert method and custom iterators.
    Also clean up implementation and extend testing source file.

--- (Dynamic) Array/include/array.h
+++ (Dynamic) Array/include/array.h
@@ -9,8 +9,10 @@ class Array
 {
     private:
         T* entries;
-        size_t count;
-        size_t capacity;
+        size_t _count;
+        size_t _capacity;
+
+        void shift(int shift, int start = 0);
 
     public:
         // Constructing Array objects.
@@ -30,20 +32,64 @@ class Array
         void grow();
 
         void add(T element);
+        void insert(T element, int index);
         T& pop();
-        size_t size();
-        size_t maxSize();
-        T* begin();
-        T* end();
-        T& raw(int index); // index < capacity.
+        size_t count();
+        size_t capacity();
+        T* front();
+        T* back();
+        T& slot(int index); // index < capacity.
+
+        class iterator
+        {
+            private:
+                T* ptr;
+            
+            public:
+                iterator(T* ptr);
+                iterator(const iterator& other);
+                iterator& operator=(const iterator& other);
+
+                T& operator*() const;
+                iterator& operator++();
+                iterator operator++(int);
+                iterator& operator--();
+                iterator operator--(int);
+                bool operator==(const iterator& other);
+                bool operator!=(const iterator& other);
+        };
+
+        class const_iterator
+        {
+            private:
+                const T* ptr;
+            
+            public:
+                const_iterator(const T* ptr);
+                const_iterator(const const_iterator& other);
+                const_iterator& operator=(const const_iterator& other);
+
+                const T& operator*() const;
+                const_iterator& operator++();
+                const_iterator operator++(int);
+                const_iterator& operator--();
+                const_iterator operator--(int);
+                bool operator==(const const_iterator& other) const;
+                bool operator!=(const const_iterator& other) const;
+        };
+
+        iterator begin();
+        iterator end();
+        const_iterator cbegin();
+        const_iterator cend();
 };
 
 TEMP
 Array<T>::Array()
 {
     entries = nullptr; // Well-defined to call delete[] on nullptr.
-    count = 0;
-    capacity = 0;
+    _count = 0;
+    _capacity = 0;
 }
 
 TEMP
@@ -52,40 +98,45 @@ Array<T>::Array(int size)
     if (size < 0)
         throw std::out_of_range("Negative index cannot be used.");
     entries = new T[size];
-    count = (size_t) size;
-    capacity = (size_t) size;
+    _count = (size_t) 0; // No elements used at time of construction.
+    _capacity = (size_t) size;
 }
 
 TEMP
 Array<T>::Array(const Array<T>& other)
 {
-    this->entries = new T[other.count];
-    for (int i = 0; i < (int) this->count; i++)
+    this->entries = new T[other._capacity];
+
+    for (int i = 0; i < (int) this->_count; i++)
         this->entries[i] = other.entries[i];
-    this->count = other.count;
-    this->capacity = other.capacity;
+
+    this->_count = other._count;
+    this->_capacity = other._capacity;
 }
 
 TEMP
 Array<T>::Array(Array<T>&& other)
 {
     this->entries = other.entries;
-    this->count = other.count;
-    this->capacity = other.capacity;
+    this->_count = other._count;
+    this->_capacity = other._capacity;
 
     other.entries = nullptr;
-    other.count = 0;
-    other.capacity = 0;
+    other._count = 0;
+    other._capacity = 0;
 }
 
 TEMP
 Array<T>& Array<T>::operator=(const Array<T>& other)
 {
     delete[] this->entries;
-    for (int i = 0; i < (int) this->count; i++)
+    this->entries = new T[other._capacity];
+
+    for (int i = 0; i < (int) this->_count; i++)
         this->entries[i] = other.entries[i];
-    this->count = other.count;
-    this->capacity = other.capacity;
+
+    this->_count = other._count;
+    this->_capacity = other._capacity;
     return *this;
 }
 
@@ -95,12 +146,12 @@ Array<T>& Array<T>::operator=(Array<T>&& other)
     delete[] this->entries;
 
     this->entries = other.entries;
-    this->count = other.count;
-    this->capacity = other.capacity;
+    this->_count = other._count;
+    this->_capacity = other._capacity;
 
     other.entries = nullptr;
-    other.count = 0;
-    other.capacity = 0;
+    other._count = 0;
+    other._capacity = 0;
 
     return *this;
 }
@@ -108,15 +159,15 @@ Array<T>& Array<T>::operator=(Array<T>&& other)
 TEMP
 Array<T>::~Array()
 {
-    count = 0;
-    capacity = 0;
+    _count = 0;
+    _capacity = 0;
     delete[] entries;
 }
 
 TEMP
 T& Array<T>::operator[](int index)
 {
-    if ((index < 0) || ((size_t) index >= count))
+    if ((index < 0) || ((size_t) index >= _count))
         throw std::out_of_range("Invalid index.");
 
     return entries[index];
@@ -125,9 +176,9 @@ T& Array<T>::operator[](int index)
 TEMP
 bool Array<T>::operator==(const Array<T>& other)
 {
-    if (this->count != other.count) return false;
+    if (this->_count != other._count) return false;
 
-    for (int i = 0; i < (int) this->count; i++)
+    for (int i = 0; i < (int) this->_count; i++)
     {
         if (this->entries[i] != other.entries[i])
             return false;
@@ -139,58 +190,266 @@ bool Array<T>::operator==(const Array<T>& other)
 TEMP
 void Array<T>::grow()
 {
-    capacity = (capacity == 0 ? 8 : capacity * 2);
-    T* newEntries = new T[capacity];
-    for (int i = 0; i < (int) count; i++)
+    _capacity = (_capacity == 0 ? 8 : _capacity * 2);
+    T* newEntries = new T[_capacity];
+    for (int i = 0; i < (int) _count; i++)
         newEntries[i] = std::move(entries[i]);
     delete[] entries;
     this->entries = newEntries;
+    // this->count does not change.
     // newEntries goes out of scope here.
 }
 
 TEMP
 void Array<T>::add(T element)
 {
-    if (capacity <= count)
+    if (_capacity <= _count)
         grow();
-    entries[(int) count++] = element;
+    entries[(int) _count++] = element;
+}
+
+// When using this function, the unshifted
+// buffer area in the array must be replaced/
+// initialized immediately to maintain
+// the contiguous storage of elements in the array.
+TEMP
+void Array<T>::shift(int shift, int start)
+{
+    if ((shift == 0) || (_count == 0)) // Nothing to do.
+        return;
+
+    if (shift < 0)
+        return; // Throw error?
+
+    if ((start < 0) || (start >= _count))
+        return; // Throw error?
+
+    // Shift might be large, so we
+    // repeatedly grow until capacity is enough.
+    while (_capacity < _count + shift)
+        grow();
+    
+    // To avoid data corruption, we make a
+    // new internal array.
+    T* newEntries = new T[_capacity];
+    for (int i = 0; i < start; i++)
+        newEntries[i] = this->entries[i];
+    
+    for (int i = start; i < _count; i++)
+        newEntries[i + shift] = this->entries[i];
+    delete[] this->entries;
+    this->entries = newEntries;
+    // Only increment count when we fill the
+    // empty spots.
+}
+
+TEMP
+void Array<T>::insert(T element, int index)
+{
+    if ((index < 0) || (index >= _count))
+        return; // Throw error?
+    
+    if (_capacity <= _count)
+        grow();
+
+    shift(1, index);
+    entries[index] = element;
+    _count++;
 }
 
 TEMP
 T& Array<T>::pop()
 {
-    count--;
-    return entries[(int) count];
+    _count--;
+    return entries[(int) _count];
 }
 
 TEMP
-size_t Array<T>::size()
+size_t Array<T>::count()
 {
-    return count;
+    return _count;
 }
 
 TEMP
-size_t Array<T>::maxSize()
+size_t Array<T>::capacity()
 {
-    return capacity;
+    return _capacity;
 }
 
 TEMP
-T* Array<T>::begin()
+T* Array<T>::front()
 {
     return entries;
 }
 
 TEMP
-T* Array<T>::end()
+T* Array<T>::back()
 {
-    return entries + count;
+    return entries + _count;
 }
 
 TEMP
-T& Array<T>::raw(int index)
+T& Array<T>::slot(int index)
 {
-    if ((index < 0) || ((size_t) index >= capacity))
+    if ((index < 0) || ((size_t) index >= _capacity))
         throw std::out_of_range("Invalid index.");
     return entries[index];
+}
+
+// Iterator implementation.
+
+#define arrIter Array<T>::iterator
+
+TEMP
+arrIter::iterator(T* ptr) :
+    ptr(ptr) {}
+
+TEMP
+arrIter::iterator(const arrIter& other) :
+    ptr(other.ptr) {}
+
+TEMP
+typename arrIter& arrIter::operator=(const arrIter& other)
+{
+    this->ptr = other.ptr;
+    return *this;
+}
+
+TEMP
+T& arrIter::operator*() const
+{
+    return *ptr;
+}
+
+TEMP
+typename arrIter& arrIter::operator++()
+{
+    ++ptr;
+    return *this;
+}
+
+TEMP
+typename arrIter arrIter::operator++(int n)
+{
+    arrIter temp = *this;
+    ptr++;
+    return temp;
+}
+
+TEMP
+typename arrIter& arrIter::operator--()
+{
+    --ptr;
+    return *this;
+}
+
+TEMP
+typename arrIter arrIter::operator--(int n)
+{
+    arrIter temp = *this;
+    ptr--;
+    return temp;
+}
+
+TEMP
+bool arrIter::operator==(const arrIter& other)
+{
+    return (this->ptr == other.ptr);
+}
+
+TEMP
+bool arrIter::operator!=(const arrIter& other)
+{
+    return (this->ptr != other.ptr);
+}
+
+TEMP
+typename arrIter Array<T>::begin()
+{
+    return iterator(front());
+}
+
+TEMP
+typename arrIter Array<T>::end()
+{
+    return iterator(back());
+}
+
+// Const iterator implementation.
+
+#define constIter Array<T>::const_iterator
+
+TEMP
+constIter::const_iterator(const T* ptr) :
+    ptr(ptr) {}
+
+TEMP
+constIter::const_iterator(const const_iterator& other) :
+    ptr(other.ptr) {}
+
+TEMP
+typename constIter& constIter::operator=(const const_iterator& other)
+{
+    this->ptr = other.ptr;
+    return *this;
+}
+
+TEMP
+const T& constIter::operator*() const
+{
+    return *ptr;
+}
+
+TEMP
+typename constIter& constIter::operator++()
+{
+    ++ptr;
+    return *this;
+}
+
+TEMP
+typename constIter constIter::operator++(int n)
+{
+    constIter temp = *this;
+    ptr++;
+    return temp;
+}
+
+TEMP
+typename constIter& constIter::operator--()
+{
+    --ptr;
+    return *this;
+}
+
+TEMP
+typename constIter constIter::operator--(int n)
+{
+    constIter temp = *this;
+    ptr--;
+    return *this;
+}
+
+TEMP
+bool constIter::operator==(const const_iterator& other) const
+{
+    return (this->ptr == other.ptr);
+}
+
+TEMP
+bool constIter::operator!=(const const_iterator& other) const
+{
+    return (this->ptr != other.ptr);
+}
+
+TEMP
+typename constIter Array<T>::cbegin()
+{
+    return const_iterator(front());
+}
+
+TEMP
+typename constIter Array<T>::cend()
+{
+    return const_iterator(end());
 }
--- (Dynamic) Array/src/test.cpp
+++ (Dynamic) Array/src/test.cpp
@@ -4,20 +4,21 @@
 int main()
 {
     Array<int> array0;
-    std::cout << "Array 0 Size: " << array0.size() << '\n';
-    std::cout << "Array 0 Capacity: " << array0.maxSize() << '\n';
+    std::cout << "Array 0 Size: " << array0.count() << '\n';
+    std::cout << "Array 0 Capacity: " << array0.capacity() << '\n';
 
     Array<int> array1(0);
     for (int i = 0; i < 10; i++)
         array1.add(i + 1);
-    for (int* i = array1.begin(); i < array1.end(); i++)
-        std::cout << *i << '\n';
+    array1.insert(-1, 3);
+    for (const int i : array1)
+        std::cout << i << '\n';
     array1.pop();
     
     Array<int> array2 = array1;
-    std::cout << "Array 1 Size: " << array1.size() << '\n';
-    std::cout << "Array 1 Capacity: " << array1.maxSize() << '\n';
-    std::cout << "Array 2 Size: " << array2.size() << '\n';
-    std::cout << "Array 2 Capacity: " << array2.maxSize() << '\n';
+    std::cout << "Array 1 Size: " << array1.count() << '\n';
+    std::cout << "Array 1 Capacity: " << array1.capacity() << '\n';
+    std::cout << "Array 2 Size: " << array2.count() << '\n';
+    std::cout << "Array 2 Capacity: " << array2.capacity() << '\n';
     return 0;
 }
