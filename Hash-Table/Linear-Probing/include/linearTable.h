5072f14b360f122c1744205a11efa5169ab3f00b
Ali Hamza <ali.mhamza06@gmail.com>
2025-12-23 05:17:34 +0400

    Currently functional form of the linear probing hash table, with testing.
    It remains to implement some user-friendly functionality, such as a proper [] operator.
    The linear probing approach seems to give comparable or faster times relative to separate chaining (tests used 10,000 additions, retrievals, and deletions, both being within 1-3ms).

---
+++ Hash-Table/Linear-Probing/include/linearTable.h
@@ -0,0 +1,237 @@
+#pragma once
+#include "../../../Dynamic-Array/include/array.h"
+#include "../../General/include/entryStruct.h"
+#include "../../General/include/hashFunctions.h"
+#include <cstdint>
+#include <iostream> // For debugging.
+
+#define KVTEMP template<typename Key, typename Value>
+#define EKV Entry<Key, Value>
+
+KVTEMP
+class linearTable
+{
+    private:
+        // Keeping the same load factor for 
+        // both implementations.
+        static constexpr double loadFactor = 0.8;
+        Array<EKV> entries;
+        int count;
+        int maxIndex;
+
+        linearTable(int size);
+
+        void reorder();
+        void resize();
+        // Searches for existing key.
+        // Returns reference to available bucket
+        // if not found.
+        EKV& findSlot(Key key, int* pos);
+    
+    public:
+        linearTable();
+        linearTable(const linearTable<Key, Value>& other);
+        ~linearTable() = default;
+        linearTable<Key, Value>& operator=(const linearTable<Key, Value>& other);
+        Value& operator[](Key key);
+
+        void add(Key key, Value value);
+        Value* get(Key key);
+        void set(Key key, Value value);
+        void remove(Key key);
+        void merge(const linearTable<Key, Value>& other);
+        int size();
+
+        // For debugging.
+        void printTable();
+};
+
+KVTEMP
+linearTable<Key, Value>::linearTable() :
+    entries(2), count(0), maxIndex(-1) {}
+
+KVTEMP
+linearTable<Key, Value>::linearTable(const linearTable<Key, Value>& other) :
+    entries(other.entries), count(other.count),
+    maxIndex(other.maxIndex) {}
+
+KVTEMP
+linearTable<Key, Value>::linearTable(int size) :
+    entries(size), count(0), maxIndex(-1) {}
+
+KVTEMP
+linearTable<Key, Value>& linearTable<Key, Value>::
+operator=(const linearTable<Key, Value>& other)
+{
+    this->entries = other.entries;
+    return *this;
+}
+
+KVTEMP
+void linearTable<Key, Value>::reorder()
+{
+    size_t capacity = entries.capacity();
+    linearTable<Key, Value> newTable(static_cast<int>(capacity));
+    for (size_t i = 0; i < capacity; i++)
+    {
+        EKV entry = entries.slot((int) i);
+        if (entry.state != VALID)
+            continue;
+        newTable.add(entry.key, entry.value);
+    }
+    this->entries = newTable.entries;
+    this->count = newTable.count; // Might have changed since we dropped tombstones.
+    this->maxIndex = newTable.maxIndex;
+}
+
+KVTEMP
+void linearTable<Key, Value>::resize()
+{   
+    if ((entries.capacity() * loadFactor) < count + 1)
+    {
+        if (maxIndex != -1)
+            setCount(this->entries, (size_t) (maxIndex + 1));
+        entries.grow();
+    }
+    else
+        return;
+
+    // Need to re-index entire array after this.
+    // Descend into each list and extract its node
+    // elements to construct the new table.
+    
+    if (count != 0) // There are elements to reorder.
+        reorder();
+}
+
+KVTEMP
+EKV& linearTable<Key, Value>::findSlot(Key key, int* pos)
+{
+    uint32_t hash = hashKey(key);
+    uint32_t bitmask = (uint32_t) (entries.capacity() - 1);
+    int index = (int) (hash & bitmask);
+
+    EKV* tombstone = nullptr;
+
+    EKV* entry = &(entries.slot(index));
+    if (pos != nullptr)
+        *pos = index;
+    while (entry->state != EMPTY)
+    {
+        if (pos != nullptr)
+            *pos = index;
+        
+        if (entry->key == key)
+            return *entry;
+        
+        if (entry->state == TOMBSTONE)
+            tombstone = entry;
+
+        index = (index + 1) & bitmask;
+        if (pos != nullptr)
+            *pos = index;
+        entry = &(entries.slot(index));
+    }
+
+    // We didn't find an entry with the key.
+    // If we found a tombstone, return it (to reuse it).
+    // Otherwise, return the first empty slot we found.
+    return (tombstone == nullptr ? *entry : *tombstone);
+}
+
+KVTEMP
+void linearTable<Key, Value>::add(Key key, Value value)
+{
+    EKV& existEntry = findSlot(key, nullptr);
+    if (existEntry.state == VALID) // Key already exists.
+    {
+        existEntry.value = value;
+        return;
+    }
+
+    resize(); // Grow size if needed.
+
+    uint32_t hash = hashKey(key);
+    int index;
+    EKV& newEntry = findSlot(key, &index);
+
+    if (newEntry.state != TOMBSTONE)
+        count++;
+
+    newEntry.key = key;
+    newEntry.value = value;
+    newEntry.hash = hash;
+    newEntry.state = VALID;
+
+    maxIndex = (maxIndex > index ? maxIndex : index);
+}
+
+KVTEMP
+Value* linearTable<Key, Value>::get(Key key)
+{
+    if (count == 0) return nullptr;
+    
+    EKV& entry = findSlot(key, nullptr);
+    if (entry.state != VALID)
+        return nullptr;
+    else
+        return &(entry.value);
+}
+
+KVTEMP
+void linearTable<Key, Value>::set(Key key, Value value)
+{
+    EKV& entry = findSlot(key, nullptr);
+    if (entry.state != VALID)
+        add(key, value);
+    else
+        entry.value = value;
+}
+
+KVTEMP
+void linearTable<Key, Value>::remove(Key key)
+{
+    EKV& entry = findSlot(key, nullptr);
+    if (entry.state == VALID) // Leave it if it's already empty.
+        entry.state = TOMBSTONE;
+}
+
+KVTEMP
+void linearTable<Key, Value>::merge(const linearTable<Key, Value>& other)
+{
+    size_t capacity = other.entries.capacity();
+    for (size_t i = 0; i < capacity; i++)
+    {
+        EKV entry = other.entries.slot((int) i);
+        if (entry.state == VALID)
+            add(entry.key, entry.value);
+    }
+}
+
+KVTEMP
+int linearTable<Key, Value>::size()
+{
+    return count;
+}
+
+KVTEMP
+void linearTable<Key, Value>::printTable()
+{
+    size_t cap = entries.capacity();
+    for (size_t i = 0; i < cap; i++)
+    {
+        std::cout << "Slot " << i << ": ";
+        EKV& entry = entries.slot(i);
+        if (entry.state == EMPTY)
+            std::cout << "EMPTY\n";
+        else
+        {
+            std::cout << "(" << entry.key << ", "
+                << entry.value << ")";
+            if (entry.state == TOMBSTONE)
+                std::cout << " (TOMB)\n";
+            else
+                std::cout << '\n';
+        }
+    }
+}
--- Hash-Table/Linear-Probing/src/test.cpp
+++ Hash-Table/Linear-Probing/src/test.cpp
@@ -1,5 +1,6 @@
 #include "../include/linearTable.h"
 #include <iostream>
+#include <chrono>
 
 int main()
 {
@@ -12,13 +13,30 @@ int main()
 
     std::cout << "Size: " << table.size() << '\n';
     for (int i = 1; i < 11; i++)
-        std::cout << table.get((char)(i + 64)) << '\n';
+        std::cout << *table.get((char)(i + 64)) << '\n';
 
-    table.add((char) 75, 24);
-    std::cout << (char) 75 << ":" << *table.get((char) 75) << '\n';
-    table.remove((char) 75);
-    if (table.get((char) 75) == nullptr)
+    table.add((char) 65, 24);
+    std::cout << (char) 65 << ":" << *table.get((char) 65) << '\n';
+    table.remove((char) 65);
+    if (table.get((char) 65) == nullptr)
         std::cout << "Removed key successfully.\n";
+
+    using namespace std::chrono;
+
+    linearTable<int, int> table2;
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < 500; i++)
+        table2.add(i, i + 1);
+
+    for (int i = 0; i < 500; i++)
+        std::cout << *table2.get(i) << '\n';
+
+    for (int i = 0; i < 500; i++)
+        table2.remove(i);
+
+    auto finish = high_resolution_clock::now();
+    auto time = duration_cast<milliseconds>(finish - start);
+    std::cout << "Time: " << (long double)time.count() / 1000 << '\n';
 
     return 0;
 }
