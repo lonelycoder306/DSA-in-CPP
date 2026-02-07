8408decb364c144affb90638e8ca7a2956a6a4e9
Ali Hamza <ali.mhamza06@gmail.com>
2026-02-07 12:43:20 +0400

    Correct potential overflow in binary search algorithm.

--- Algorithms/Searching/binary.h
+++ Algorithms/Searching/binary.h
@@ -7,7 +7,7 @@ int search(T* elems, int count, T find)
     int end = count - 1;
     while (end >= begin)
     {
-        int mid = (begin + end) / 2;
+        int mid = begin + (end - begin)/2;
         if (elems[mid] == find)
             return mid;
         else if (elems[mid] > find)
